92c4a9fe1c5e322aa3a07bb73b510843e0c8f4af
Mathieu Antoine <mathieuantoine4@gmail.com>
2025-03-10 19:53:14 +0100

    removed unused file

--- src/services/favorites.service.ts
+++
@@ -1,9 +0,0 @@
-export function getFavorites() {
-    const favorites = window.localStorage.getItem('favorites')
-    if(!favorites) return []
-    else return JSON.parse(favorites)
-}
-
-export function setFavorites(favorites: string[]) {
-    window.localStorage.setItem('favorites', JSON.stringify(favorites))
-}
